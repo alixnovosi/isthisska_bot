f508441a9f97fe394d16cbbb99581c4eb56e27dd
Andrew Michaud <dev@mail.andrewmichaud.com>
2016-11-06 01:18:51 -0800

    license, version, setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from os import path
+from setuptools import setup, find_packages
+
+HERE = path.abspath(path.dirname(__file__))
+
+with open(path.join(HERE, "VERSION"), encoding="utf-8") as f:
+    VERSION = f.read().strip()
+
+setup(author="Andrew Michaud",
+      author_email="bots+isthisska@mail.andrewmichaud.com",
+
+      entry_points={
+          "console_scripts": ["isthisska_bot = isthisska_bot.__main__:main"]
+      },
+
+      install_requires=["tweepy>=3.5", "bs4"],
+
+      license="BSD3",
+
+      name="isthisska_bot",
+
+      packages=find_packages(),
+
+      # Project"s main homepage
+      url="https://github.com/andrewmichaud/isthisska_bot",
+
+      version=VERSION)
