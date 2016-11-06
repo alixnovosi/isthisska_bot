453b657387398b897a50dc852344700adaf15e10
Andrew Michaud <dev@mail.andrewmichaud.com>
2016-11-06 01:15:40 -0800

    Bring over sending logic.
    Generate art, send tweet with art, sleep.

---
+++ isthisska_bot/__main__.py
@@ -0,0 +1,26 @@
+"""Main class for bot."""
+
+import time
+
+import album_art_gen
+import send
+
+# Delay between tweets in seconds.
+DELAY = 3600
+
+if __name__ == "__main__":
+    api = send.auth_and_get_api()
+
+    with open("log", "w") as f:
+        while True:
+            f.write("Grabbing random album art from Musicbrainz.")
+            f.flush()
+            album_art_gen.produce_random_album_art()
+
+            f.write("Sending tweet with art.")
+            f.flush()
+            api.update_with_media("album_art.jpg", status="Is this ska?")
+
+            f.write("Sleeping for {} seconds.\n".format(DELAY))
+            f.flush()
+            time.sleep(DELAY)
