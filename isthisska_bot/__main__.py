0a1501be5739193e62178f1b6722081d40a76db4
Andrew Michaud <dev@mail.andrewmichaud.com>
2016-11-11 02:12:05 -0800

    Add rate limiting around API calls.
    Refactor for cleanliness.
    Break album art generation into more pieces.
    Introduce util class for the rate limiting, and move logging there.
    Fix logs having newlines in them.
    Don't completely break on API exceptions - try to handle them and then
    retry.

--- isthisska_bot/__main__.py
+++ isthisska_bot/__main__.py
@@ -1,53 +1,36 @@
 """Main class for bot."""
 
-import logging
 import subprocess
-import sys
 import time
-from logging.handlers import RotatingFileHandler
+
+import tweepy
 
 import album_art_gen
 import send
-import tweepy
+import util
 
 # Delay between tweets in seconds.
 DELAY = 3600
 ALBUM_ART_FILENAME = album_art_gen.ALBUM_ART_FILENAME
 TWEET_TEXT = "Is this ska?"
 
-
-def set_up_logging():
-    """Set up proper logging."""
-    logger = logging.getLogger("root")
-    logger.setLevel(logging.DEBUG)
-
-    # Log everything verbosely to a file.
-    file_handler = RotatingFileHandler(filename="log", maxBytes=1024000000, backupCount=10)
-    verbose_form = logging.Formatter(fmt="%(asctime)s - %(levelname)s - %(module)s - %(message)s")
-    file_handler.setFormatter(verbose_form)
-    file_handler.setLevel(logging.DEBUG)
-    logger.addHandler(file_handler)
-
-    # Provide a stdout handler logging at INFO.
-    stream_handler = logging.StreamHandler(sys.stdout)
-    simple_form = logging.Formatter(fmt="%(message)s")
-    stream_handler.setFormatter(simple_form)
-    stream_handler.setLevel(logging.INFO)
-
-    logger.addHandler(stream_handler)
-
-    return logger
-
 if __name__ == "__main__":
     api = send.auth_and_get_api()
 
-    LOG = set_up_logging()
+    LOG = util.set_up_logging()
 
     while True:
-        LOG.info("Grabbing random album art from Musicbrainz\n.")
-        album_art_gen.produce_random_album_art()
+        LOG.info("Grabbing random album art from Musicbrainz.")
+        try:
+            album_art_gen.produce_random_album_art()
 
-        LOG.info("Sending tweet with art\n.")
+        except album_art_gen.APIException as e:
+            LOG.error("Encountered an API Exception.")
+            LOG.error("Code: %s Message: %s", e.code, e.message)
+            LOG.error("Restarting from the beginning.")
+            continue
+
+        LOG.info("Sending tweet with art.")
 
         try:
             api.update_with_media(ALBUM_ART_FILENAME, status=TWEET_TEXT)
@@ -55,20 +38,20 @@ if __name__ == "__main__":
         except tweepy.TweepError as e:
             if hasattr(e, "reason") and "File is too big" in e.reason:
                 # Image is too big to tweet. Shrink and retry.
-                LOG.info("Running shrink commands.\n")
+                LOG.info("Running shrink commands.")
                 subprocess.run(["convert", ALBUM_ART_FILENAME, "-resize", "50%",
                                 "smaller" + ALBUM_ART_FILENAME])
                 subprocess.run(["mv", "smaller" + ALBUM_ART_FILENAME, ALBUM_ART_FILENAME])
 
-                LOG.info("Retrying tweet.\n")
+                LOG.info("Retrying tweet.")
 
                 api.update_with_media(ALBUM_ART_FILENAME, status=TWEET_TEXT)
 
             else:
-                LOG.critical("A Tweepy error we don't know how to handle happened.\n")
+                LOG.critical("A Tweepy error we don't know how to handle happened.")
                 LOG.critical("Error reason: {}".format(e.reason))
                 LOG.critical("Exiting.")
                 break
 
-        LOG.info("Sleeping for {} seconds.\n".format(DELAY))
+        LOG.info("Sleeping for {} seconds.".format(DELAY))
         time.sleep(DELAY)
--- isthisska_bot/album_art_gen.py
+++ isthisska_bot/album_art_gen.py
@@ -4,35 +4,28 @@ import random
 import requests
 from bs4 import BeautifulSoup
 
-MB_API_ROOT = "http://musicbrainz.org/ws/2/"
+import util
+
+
+# CONSTANTS related to the APIs we need to use.
+MB_API_ROOT = "https://musicbrainz.org/ws/2/"
 RELEASE_ROOT = MB_API_ROOT + "release/?query="
 
-COVER_ART_API_URL = "http://coverartarchive.org/release"
+COVER_ART_API_URL = "https://coverartarchive.org/release"
 
+# Hits per hour.
+MB_RATE_LIMIT = 1800
+
+# Being a good citizen - produce a useful user_agent.
 OWNER_URL = "https://github.com/andrewmichaud/isthisska_bot"
 USER_AGENT = "isthisska_twitterbot/1.0 (" + OWNER_URL + ") (bots+isthisska@mail.andrewmichaud.com)"
 HEADERS = {"User-Agent": USER_AGENT}
 
+# For caching(ish) results from MusicBrainz, and storing our eventual output image.
 RELEASE_COUNT_DICT = {}
-
 ALBUM_ART_FILENAME = "album_art.jpg"
 
 LOG = logging.getLogger("root")
-
-
-def gen_dict(letter):
-    """Generate count of releases in musicbrainz for letter search."""
-    url = RELEASE_ROOT + "{}&limit=1".format(letter)
-    LOG.debug("URL to get count: {}.".format(url))
-    resp = requests.get(url, headers=HEADERS)
-
-    soup = BeautifulSoup(resp.text, "html.parser")
-    release_list = soup.find("release-list")
-    count = release_list.attrs["count"]
-
-    LOG.debug("Got count {}.".format(count))
-
-    RELEASE_COUNT_DICT[letter] = count
 
 
 def produce_random_album_art():
@@ -48,20 +41,21 @@ def produce_random_album_art():
     count = RELEASE_COUNT_DICT[letter]
 
     # Try a few times - not all albums MusicBrainz lists has album art.
-    for i in range(1):
+    for i in range(15):
 
-        # Choose a random index into the releases.
-        offset = random.choice(range(int(count) - 1))
-        url = "{}{}&limit=1&offset={}".format(RELEASE_ROOT, letter, offset)
-        LOG.debug("URL to get a random release: {}.".format(url))
-        resp = requests.get(url, headers=HEADERS)
+        # Get a random release.
+        resp = perform_random_release_search(letter, count)
         if resp.status_code == 503:
-            LOG.critical("rate limited! STOP!")
-            raise Exception
+            msg = "Rate limited, or other service error."
+            LOG.error(msg)
+            raise APIException(msg, resp.status_code)
 
         elif resp.status_code != 200:
-            LOG.critcal("Unhandled error - code {}. Exiting.".format(resp.status_code))
-            raise Exception
+            msg = "Unhandled error - code {}. Exiting.".format(resp.status_code)
+            LOG.error(msg)
+            raise APIException(msg, resp.status_code)
+
+        LOG.info("Performed release search, no issues.")
 
         # who needs error handling.
         # Get release MBID to pull up the album art.
@@ -69,15 +63,12 @@ def produce_random_album_art():
         release = soup.find("release")
         LOG.debug("Release: {}".format(release))
 
-        id = release.attrs["id"]
-        LOG.info("Got release id {}.".format(id))
+        release_id = release.attrs["id"]
+        LOG.info("Got release id {}.".format(release_id))
 
         # Get an image - at last!
-        # Hit coverartarchive API to get the actual art.
-        url = "{}/{}".format(COVER_ART_API_URL, id)
-        LOG.debug("Cover art API url: {}".format(url))
-        resp = requests.get(url, headers=HEADERS)
 
+        resp = perform_album_art_search(release_id)
         if resp.status_code == 404:
             LOG.error("Got 404 trying to get album art!")
             continue
@@ -87,7 +78,77 @@ def produce_random_album_art():
         album_art_url = data["images"][0]["image"]
 
         with open(ALBUM_ART_FILENAME, "wb") as f:
-            art_data = requests.get(album_art_url, headers=HEADERS)
+            art_data = get_image(album_art_url)
             f.write(art_data.content)
 
         break
+
+
+def gen_dict(letter):
+    """Generate count of releases in musicbrainz for letter search."""
+    resp = perform_letter_search(letter)
+    soup = BeautifulSoup(resp.text, "html.parser")
+
+    release_list = soup.find("release-list")
+    count = release_list.attrs["count"]
+
+    LOG.debug("Got count {}.".format(count))
+
+    RELEASE_COUNT_DICT[letter] = count
+
+
+# Wrappers around API stuff - handle URLs and logic for how we hit MusicBrainz and Cover Art
+# Archive.
+def get_image(url):
+    """Get image from URL we got from album art search."""
+    return cover_art_archive_query(url)
+
+
+def perform_album_art_search(release_id):
+    """Get an actual piece of album art."""
+    # Hit coverartarchive API to get the actual art.
+    url = "{}/{}".format(COVER_ART_API_URL, release_id)
+    LOG.debug("Cover art API url: {}".format(url))
+    return cover_art_archive_query(url)
+
+
+def perform_random_release_search(letter, count):
+    """
+    Get a random release from MusicBrainz.
+    Do this by randomly pulling a release from the list returned by searching for a letter.
+    """
+    # Choose a random index into the releases.
+    offset = random.choice(range(int(count) - 1))
+    url = "{}{}&limit=1&offset={}".format(RELEASE_ROOT, letter, offset)
+    LOG.debug("URL to get a random release: {}.".format(url))
+    return mb_query(url)
+
+
+def perform_letter_search(letter):
+    """Get results of search for a letter from MusicBrainz API."""
+    url = RELEASE_ROOT + "{}&limit=1".format(letter)
+    LOG.debug("URL to get count: {}.".format(url))
+    return mb_query(url)
+
+
+# Rate-limited functions to do the actual hitting.
+# There's no rate limit on the Cover Art Archive, but be nice and stay at or below 2 requests a
+# second.
+@util.rate_limited(MB_RATE_LIMIT)
+def cover_art_archive_query(url, headers=HEADERS):
+    """Perform rate-limited query against Cover Art Archive API."""
+    return requests.get(url, headers=HEADERS)
+
+
+@util.rate_limited(MB_RATE_LIMIT)
+def mb_query(url, headers=HEADERS):
+    """Perform rate-limited query against MusicBrainz API."""
+    return requests.get(url, headers=headers)
+
+
+# Other stuff.
+class APIException(Exception):
+    def __init__(self, message, code):
+        super(APIException, self).__init__(message)
+
+        self.code = code
