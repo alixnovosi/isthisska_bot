8be4bbea80c8de4f08d5621da69c77cc77437aec
Andrew Michaud <dev@mail.andrewmichaud.com>
2017-12-14 12:47:54 -0800

    latest botskeleton

--- isthisska_bot/__main__.py
+++ isthisska_bot/__main__.py
@@ -19,8 +19,7 @@ MAX_IMAGE_SIZE_BYTES = 3072 * 1024
 if __name__ == "__main__":
     SECRETS_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), "SECRETS")
     BOT_SKELETON = botskeleton.BotSkeleton(SECRETS_DIR, bot_name="isthisska_bot", delay=DELAY)
-
-    LOG = botskeleton.set_up_logging()
+    LOG = BOT_SKELETON.log
 
     while True:
         LOG.info("Grabbing random album art from Musicbrainz.")
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(author="Andrew Michaud",
           "console_scripts": ["isthisska_bot = isthisska_bot.__main__:main"]
       },
 
-      install_requires=["backoff>=1.3.1", "botskeleton>=1.2.2", "bs4", "tweepy>=3.5", "Pillow"],
+      install_requires=["backoff>=1.3.1", "botskeleton>=2.0.0", "bs4", "tweepy>=3.5", "Pillow"],
       python_requires=">=3.6",
 
       license="BSD3",
